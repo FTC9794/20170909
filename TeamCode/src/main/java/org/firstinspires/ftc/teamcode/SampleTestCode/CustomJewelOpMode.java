2eeba47f2934d6be56069dc0b54666d64029cc82
RollerCoaster45 <ishaanoberoi@gmail.com>
2018-01-28 19:36:40 -0500

    Created rough jewel detection.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleTestCode/CustomJewelOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleTestCode/CustomJewelOpMode.java
@@ -3,11 +3,13 @@ package org.firstinspires.ftc.teamcode.SampleTestCode;
 import com.disnodeteam.dogecv.CameraViewDisplay;
 import com.disnodeteam.dogecv.detectors.CustomJewelDetector;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 /**
  * Created by ishaa on 1/28/2018.
  */
 
+@TeleOp(name = "jewel detector")
 public class CustomJewelOpMode extends OpMode {
     CustomJewelDetector detector;
     @Override
